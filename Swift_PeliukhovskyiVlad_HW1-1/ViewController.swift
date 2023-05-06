ed256d568a72d87015a75581d2a1f18767f2b73d
ko4i <vladi40k@icloud.com>
2023-05-06 17:24:46 +0200

    Initial Commit

---
+++ Swift_PeliukhovskyiVlad_HW1-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_PeliukhovskyiVlad_HW1-1
+//
+//  Created by Vladyslav Peliukhovskyi on 06.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
